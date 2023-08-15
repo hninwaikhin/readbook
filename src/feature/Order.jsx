d5adcfadde3be34d46390a6fd4e8854b157062a0
KHINHNIN WAI <khinhninwai@KHINHNINs-MacBook-Air.local>
2023-08-16 00:06:40 +0900

    add order

--- src/feature/Order.jsx
+++ src/feature/Order.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import MainTitle from "../components/MainTitle";
 
 export function Order(props) {
 
-    return(
-        <div>
-            Order
-        </div>
+    return (
+        <>
+            <div className="relative mt-8 ml-8 w-[1200px] h-[700px]">
+                <MainTitle title="Your Order" showHomeIcon={true} />
+               
+            </div>
+        </>
     );
 }
