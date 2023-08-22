d2dd15b39c52af883ef215cd5fdb8fc53eb7de64
RDGADM\hninkh <hnin.khin@dgshape.com>
2023-08-22 10:14:46 +0900

    delete

--- src/components/AccountSetting.jsx
+++ src/components/AccountSetting.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { navigateToUserRegister } from "../common/Variable";
 
